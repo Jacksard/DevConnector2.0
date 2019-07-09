55d0595834f7d99adba162cd4b09dd1f113fac69
Jacob Goskha <jacobgoskha@gmail.com>
2019-07-09 19:36:29 -0400

    delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -188,7 +188,7 @@ router.put(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
+    // Decustruction
     const {
       title,
       company,
@@ -206,7 +206,7 @@ router.put(
       from: from,
       to: to,
       current: current,
-      descriptiom: description
+      description: description
     };
 
     try {
@@ -221,5 +221,26 @@ router.put(
     }
   }
 );
+
+// @route     DELETE api/profile/experience/:exp_id
+// @desc      Delete experience from profile
+// @access    Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
